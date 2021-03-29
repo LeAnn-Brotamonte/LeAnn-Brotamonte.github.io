c3446e1c48c469b6f4cddfd83d79628a0c041841
LeAnn-Brotamonte <74388557+LeAnn-Brotamonte@users.noreply.github.com>
2021-03-29 17:53:27 -0500

    Add files via upload

--- jsFunctions.js
+++ jsFunctions.js
@@ -4,14 +4,14 @@ let ctx = paint.getContext("2d");
 let x = 0;
 let y = 0;
 let rect = paint.getBoundingClientRect();
-let style ="#000";
+let style ="#FF0000";
 let isDrawing = false;
 var myVar; // Variable for timer
 
 //Mouse events
 paint.addEventListener('mousedown', e => {
     x = e.clientX - rect.left;
-    y = e.clientY - rect.top; // to get the cursor position relative to the canvase
+    y = e.clientY - rect.top; // to get the cursor position relative to the "origin" of the canvas
     isDrawing = true;
 })
 
@@ -32,8 +32,8 @@ paint.addEventListener('mouseup', e => {
 })
 
 //Functions for the different buttons
-function BlackPen(){
-    style = "#000";
+function DrawPen(){
+    style = "#FF0000"; 
 }
 function Erase(){
     style = "#F5F5F5";
@@ -42,16 +42,18 @@ function ClearPaint(){
     ctx.clearRect(0,0,paint.width,paint.height);
 }
 
-function startBtn() {
-    myVar = setInterval(saveCanvas,500); // Calls saveCanvas every 3000 ms
+function displayBtn() {
+    //myVar = setInterval(saveCanvas,500); // Calls saveCanvas every 500 ms or 0.5 sec
+    saveCanvas();
 }
+// This save function was copied from the following video https://www.youtube.com/watch?v=YoVJWZrS2WU&t=343s
 function saveCanvas () {
     const a = document.createElement("a");
-    document.body.appendChild(a);
+    document.body.appendChild(a); // we are going to add the URL to the anchortag
     a.href = paint.toDataURL();
-    a.download = "userDesign.bmp";
-    a.click();
-    document.body.removeChild(a);
+    a.download = "userDesign.png";
+    a.click(); // virtually click to download
+    document.body.removeChild(a); // delete a to create new one each time
 }
 
 //Drawing function
